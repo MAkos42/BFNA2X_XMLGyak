74419f4284d55ea821301310f2ede02965d8b7d1
MAkos42 <winnerduo@gmail.com>
2022-11-09 11:28:50 +0100

    1109 óra anyaga feltöltve

---
+++ BFNA2X_1109/xPathBFNA2X/src/DomModifyNFNA2X.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.*;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+public class DomModifyNFNA2X {
+
+    public static void main(String argv[]) {
+        File xmlFile = new File("BFNA2X_1109/carsBFNA2X.xml");
+        Document doc = null;
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = factory.newDocumentBuilder();
+            doc = dBuilder.parse(xmlFile);
+
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+        }
+
+        doc.getDocumentElement().normalize();
+
+        Node cars = doc.getFirstChild();
+
+        Node supercar = doc.getElementsByTagName("supercars").item(0);
+
+        NamedNodeMap attr = supercar.getAttributes();
+        Node nodeAttr = attr.getNamedItem("company");
+        nodeAttr.setTextContent("Lamborghini");
+
+        NodeList list = supercar.getChildNodes();
+        for(int i = 0; i < list.getLength(); i++){
+            Node node = list.item(i);
+        }
+    }
+}
